e77a36cfc8bb76b599e8fe9ec75095e688db6d5a
DELL <patilgundu161@gmail.com>
2023-01-05 12:53:49 +0530

    bitManipulation

---
+++ src/bitmanipulation/count_bits.java
@@ -0,0 +1,26 @@
+package bitmanipulation;
+
+import java.util.Scanner;
+
+public class count_bits {
+    public static boolean checkBit(int x,int k){
+            if((x&(1<<k))==0){
+                return false;
+            }
+        return true;
+    }
+    public static int countBits(int A){
+        int count = 0;
+        for(int i=0 ; i<32 ; i++){
+            if(checkBit(A,i)){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(countBits(N));
+    }
+}
